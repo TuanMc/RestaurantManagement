1bfade0a561362be291a4563e633ab33d75dab81
mctuan <maichungtuan@gmail.com>
2018-07-03 12:32:16 +0700

    test

--- app/src/main/java/com/example/mctuan/restaurantmanagement/SignUpActivity.java
+++ app/src/main/java/com/example/mctuan/restaurantmanagement/SignUpActivity.java
@@ -34,6 +34,9 @@ public class SignUpActivity extends AppCompatActivity {
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
 
+//        asbacdsakdhfsdkafas
+//        asdfsldjkflsajdf
+
         btnSignIn = (Button) findViewById(R.id.sign_in_button);
         btnSignUp = (Button) findViewById(R.id.sign_up_button);
         inputEmail = (EditText) findViewById(R.id.email);
