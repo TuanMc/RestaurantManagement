1df3ed2534f558b2d5209190d172988a5fc86822
mctuan <maichungtuan@gmail.com>
2018-07-04 15:47:43 +0700

    add menu bar

---
+++ app/src/main/java/com/example/mctuan/restaurantmanagement/Activities/MenuBarActivity.java
@@ -0,0 +1,19 @@
+package com.example.mctuan.restaurantmanagement.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mctuan.restaurantmanagement.R;
+
+/**
+ * Created by mctuan on 7/4/18.
+ */
+
+public class MenuBarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_bar);
+    }
+}
